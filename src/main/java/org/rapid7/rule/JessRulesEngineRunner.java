86f9214f4277874cff7a3ddab83967bbd74c88ec
unknown <dakiva@LAX-T420-0717.tor.rapid7.com>
2012-10-03 14:04:08 -0700

    Initial project commit.

---
+++ src/main/java/org/rapid7/rule/JessRulesEngineRunner.java
@@ -0,0 +1,15 @@
+/***************************************************************************
+ * COPYRIGHT (C) 2012, Rapid7 LLC, Boston, MA, USA.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Rapid7.
+ **************************************************************************/
+package org.rapid7.rule;
+
+/**
+ *
+ * @author Daniel Akiva
+ */
+public class JessRulesEngineRunner extends RulesEngineRunner
+{
+
+}
