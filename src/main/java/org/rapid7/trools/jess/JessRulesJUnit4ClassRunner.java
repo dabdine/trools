7daf0cddb86a397694e616ced5c7f80fe7401b0f
Derek Abdine <derek.abdine@gmail.com>
2012-10-03 23:23:13 -0700

    Update formatting to adhere to coding standards.

--- src/main/java/org/rapid7/trools/jess/JessRulesJUnit4ClassRunner.java
+++ src/main/java/org/rapid7/trools/jess/JessRulesJUnit4ClassRunner.java
@@ -4,28 +4,24 @@ import org.junit.runners.model.InitializationError;
 import org.rapid7.trools.RulesJUnit4ClassRunner;
 
 /**
- * Registers and provides information about the Jess JSR-94 implementation to the JUnit framework.
+ * Registers and provides information about the Jess JSR-94 implementation to
+ * the JUnit framework.
  *
  * @author Derek Abdine
  */
-public class JessRulesJUnit4ClassRunner extends RulesJUnit4ClassRunner
-{
-   public JessRulesJUnit4ClassRunner(Class<?> klass)
-      throws InitializationError
-   {
-      super(klass);
-   }
+public class JessRulesJUnit4ClassRunner extends RulesJUnit4ClassRunner {
+    public JessRulesJUnit4ClassRunner(Class<?> klass)
+	    throws InitializationError {
+	super(klass);
+    }
 
-   @Override
-   public void registerProvider()
-      throws ClassNotFoundException
-   {
-      Class.forName(getProviderName() + ".RuleServiceProviderImpl");
-   }
+    @Override
+    public void registerProvider() throws ClassNotFoundException {
+	Class.forName(getProviderName() + ".RuleServiceProviderImpl");
+    }
 
-   @Override
-   public String getProviderName()
-   {
-      return "jess.jsr94";
-   }
+    @Override
+    public String getProviderName() {
+	return "jess.jsr94";
+    }
 }
